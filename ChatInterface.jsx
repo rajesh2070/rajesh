5e2e8f428eb6f914fee22de4b68ffe69e0795f75
rajesh2070 <73897381+rajesh2070@users.noreply.github.com>
2026-04-19 12:53:24 +0530

    Added Gujarati language support to ChatInterface component

---
+++ ChatInterface.jsx
@@ -0,0 +1,12 @@
+// Adding Gujarati language support
+const languages = {
+    en: "English",
+    hi: "हिंदी",
+    es: "Español",
+    fr: "Français",
+    de: "Deutsch",
+    zh: "中文",
+    gu: "ગુજરાતી"
+};
+
+export default languages;
